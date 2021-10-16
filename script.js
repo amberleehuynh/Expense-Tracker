6ba47df6ef05fe93950b3d054888af86f7df7091
amberlee <amberleehuynh5@gmail.com>
2021-10-16 15:28:10 -0700

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const balance = document.getElementById('balance');
+const balance = document.getElementById('balance');
